c7989058c2596eefba62ba998e3bcbb8bc2d5b6f
jhrfeng <jhrzhongyue@gmail.com>
2017-04-11 23:19:55 +0800

    还原设置

---
+++ btb/js/json/home.js
@@ -0,0 +1,4 @@
+var productList = 
+		[{name:"新人专享9611050期", shouyi:"20.0", week:"90", join:"1000元"}
+		,{name:"预约专享9601051期", shouyi:"30.0", week:"180", join:"1000元"}
+		,{name:"稀缺产品963353期", shouyi:"40.0", week:"365", join:"1000元"}];
