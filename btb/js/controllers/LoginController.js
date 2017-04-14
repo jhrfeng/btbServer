ba149269e0d9be2ca5ce7bde32d411bf2eeca66e
jhrfeng <jhrzhongyue@gmail.com>
2017-04-14 14:34:18 +0800

    手机号验证

--- btb/js/controllers/LoginController.js
+++ btb/js/controllers/LoginController.js
@@ -47,7 +47,7 @@ function($rootScope, $scope, httpUtil, $state) {
 	    }
 	    var PATTERN_CHINAMOBILE = /^1(3[4-9]|5[012789]|8[23478]|4[7]|7[8])\d{8}$/; //移动号
 	    var PATTERN_CHINAUNICOM = /^1(3[0-2]|5[56]|8[56]|4[5]|7[6])\d{8}$/; //联通号
-	    var PATTERN_CHINATELECOM = /^1(3[3])|(8[019])\d{8}$/; //电信号
+	    var PATTERN_CHINATELECOM = /^1(3[3])|1(7[3])|(8[019])\d{8}$/; //电信号
 	    if (PATTERN_CHINAUNICOM.test(newname)) {
 	        return true;
 	    } else if (PATTERN_CHINAMOBILE.test(newname)) {
