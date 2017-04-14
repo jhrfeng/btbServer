755fd19b7388f82b58c580c7f7c1b9fff0262dc7
jhrfeng <jhrzhongyue@gmail.com>
2017-04-14 14:11:26 +0800

    登录手机号限制

--- btb/js/controllers/LoginController.js
+++ btb/js/controllers/LoginController.js
@@ -28,7 +28,7 @@ function($rootScope, $scope, httpUtil, $state) {
 			alert("请输入手机号")
 			return false;
 		}
-		var phone = /^(((13[0-9]{1})|(15[0-9]{1})|(18[0-9]{1}))+\d{8})$/; 
+		var phone = /^(((13[0-9]{1})|(15[0-9]{1})|(17[0-9]{1})|(18[0-9]{1}))+\d{8})$/; 
 		if(!phone.test($scope.user.username)) 
 		{ 
 		    alert('请输入有效的手机号码！'); 
