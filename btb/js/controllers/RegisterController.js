45b07d899865834faaa7933c254bd5fbd46f7ba5
jhrfeng <jhrzhongyue@gmail.com>
2017-04-14 14:31:50 +0800

    2017手机号验证

--- btb/js/controllers/RegisterController.js
+++ btb/js/controllers/RegisterController.js
@@ -75,15 +75,8 @@ function($rootScope, $scope, httpUtil, $state) {
 	}
 	
 	function validate(){
-		if($scope.user.username=="" || null==$scope.user.username){
-			alert("请输入手机号")
+		if(!validatemobile($scope.user.username)){
 			return false;
-		}
-		var phone = /^(((13[0-9]{1})|(15[0-9]{1})|(17[0-9]{1})|(18[0-9]{1}))+\d{8})$/; 
-		if(!phone.test($scope.user.username)) 
-		{ 
-		    alert('请输入有效的手机号码！'); 
-		    return false; 
 		} 
 		if($scope.user.password=="" || null==$scope.user.password
 			|| $scope.user.passwordConfirmation=="" || null==$scope.user.passwordConfirmation){
@@ -99,6 +92,30 @@ function($rootScope, $scope, httpUtil, $state) {
 			return false;
 		}
 		return true;
+	}
+
+	function validatemobile(newname) {
+	    if (newname=='' || newname==null) {
+	        alert('请输入手机号码！');
+	        return false;
+	    }
+	    if (newname.length != 11) {
+	        alert('请输入有效的手机号！');
+	        return false;
+	    }
+	    var PATTERN_CHINAMOBILE = /^1(3[4-9]|5[012789]|8[23478]|4[7]|7[8])\d{8}$/; //移动号
+	    var PATTERN_CHINAUNICOM = /^1(3[0-2]|5[56]|8[56]|4[5]|7[6])\d{8}$/; //联通号
+	    var PATTERN_CHINATELECOM = /^1(3[3])|(8[019])\d{8}$/; //电信号
+	    if (PATTERN_CHINAUNICOM.test(newname)) {
+	        return true;
+	    } else if (PATTERN_CHINAMOBILE.test(newname)) {
+	        return true;
+	    } else if (PATTERN_CHINATELECOM.test(newname)) {
+	        return true;
+	    }else {
+	        alert("请输入正确的手机号");
+	        return false;
+	    }
 	}
 
 
