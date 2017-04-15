cfcd6984f92421fa88dc3acb970c6ca204c276a4
jhrfeng <jhrzhongyue@gmail.com>
2017-04-15 20:24:07 +0800

    收益率

--- route/home.js
+++ route/home.js
@@ -1,6 +1,6 @@
 
 // home 购买量
 exports.percenter = function(req, res) {
-	var percenter = {"0":"34.2", "1":"20.8", "2":"14.1"};
+	var percenter = {"0":"4.2", "1":"34.2", "2":"20.8", "3":"14.1"};
 	res.json(percenter);
 };
