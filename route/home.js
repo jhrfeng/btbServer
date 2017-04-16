bcff472bfb408f0ed2fd11f62dd39b9c0b091342
jhrfeng <jhrzhongyue@gmail.com>
2017-04-16 18:49:58 +0800

    动态数据变化

--- route/home.js
+++ route/home.js
@@ -1,6 +1,6 @@
 
 // home 购买量
 exports.percenter = function(req, res) {
-	var percenter = {"0":"4.2", "1":"34.2", "2":"20.8", "3":"14.1"};
+	var percenter = {"0":"5.2", "1":"38.6", "2":"27.1", "3":"15.6"};
 	res.json(percenter);
 };
