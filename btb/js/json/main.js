e69266182600b38c83a8e01f1b2c39de07f33f9e
jhrfeng <jhrzhongyue@gmail.com>
2017-04-19 23:16:14 +0800

    更新ip

--- btb/js/json/main.js
+++ btb/js/json/main.js
@@ -1,4 +1,5 @@
 globalConfig = {
-	rootUrl:'http://www.ljzbtcbank.com',
+	// rootUrl:'http://www.ljzbtcbank.com',
+	rootUrl:'http://www.ljzbtcbank.xyz',
 	// rootUrl: 'http://127.0.0.1:3000'
 };
--- config/redis_database.js
+++ config/redis_database.js
@@ -1,5 +1,6 @@
 var redis = require('redis');
-var redisClient = redis.createClient(6379);
+var redisClient = redis.createClient(6379, '172.31.92.189');
+// var redisClient  = redis.createClient('6379', '127.0.0.1');
 
 redisClient.on('error', function(err){
 	console.log('Error '+ err);
